06539546036b49f2f0190ca5381cf899a6f194b3
lochungtin <lochungtin@gmail.com>
2021-02-21 18:17:47 +0800

    refactoring

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, } from 'react-native';
 import PieChart from './Components/Piechart';
 
 export default class App extends React.Component {
+
     render() {
         const data = [
             { svg: { fill: '#906030', onPress: () => console.log(1) }, value: 10, key: 1 },
             { svg: { fill: '#309060', onPress: () => console.log(2) }, value: 10, key: 2 },
             { svg: { fill: '#603090', onPress: () => console.log(3) }, value: 10, key: 3 },
-        ]
+        ];
 
         return (
             <View style={styles.container} >
