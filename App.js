35559be33c91ebcd6617c83272d6d25e374cae67
lochungtin <lochungtin@gmail.com>
2021-02-21 18:32:31 +0800

    renaming folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, } from 'react-native';
-import PieChart from './Components/Piechart';
+import PieChart from './components/Piechart';
 
 export default class App extends React.Component {
 
